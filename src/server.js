16910f5e324fce6ddf66148c775579d55e9ce475
niwa-niwa <urban.hang274@gmail.com>
2021-09-18 14:52:33 +0900

    implemented loging

--- src/server.js
+++ src/server.js
@@ -9,8 +9,15 @@ import { isAuthenticated } from "./utils/isAuthenticated";
 
 const app = express();
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "../access.log"),
+  {
+    flags: "a",
+  }
+);
+
 app.use(helmet());
-app.use(morgan("combined"));
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
