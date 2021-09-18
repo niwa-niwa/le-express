abfbde2b7bfd6e9cb8914a588ed12feeac921507
niwa-niwa <urban.hang274@gmail.com>
2021-09-18 14:49:35 +0900

    implemented simple authentication

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import fs from "fs";
 import path from "path";
 import routes from "./routes";
+import { isAuthenticated } from "./utils/isAuthenticated";
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(morgan("combined"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use("/user", routes.user);
+app.use("/user", isAuthenticated, routes.user);
 
 app.use((req, res) => {
   res.status(404).send("404: Page not found");
